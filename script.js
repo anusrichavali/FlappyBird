65c144b1d2984d111606f5d28c777bc7956a858a
Glitch (hello-webpage) <none>
2021-07-12 20:25:40 +0000

    🚊🍉 Checkpoint ./script.js:16401033/645

--- script.js
+++ script.js
@@ -6,16 +6,34 @@
 // Content behind double slashes is a comment. Use it for plain English notes,
 // or for code that you want to temporarily disable.
 
-function setup(){
-  // Code here runs only once
-  createCanvas(800, 600);
+function setup() {
+  createCanvas(400, 400);
 }
 
-function draw(){
-  // Code here runs continuously
+function draw() {
   background(220);
-  
 
-  ellipse(50, 50, 50, 50);  
-  
+  // Brush settings
+  noFill();
+  strokeWeight(5);
+
+  // Ring 1: Blue
+  stroke(10, 134, 205);
+  ellipse(50, 50, 50);
+
+  // Ring 2: Yellow
+  stroke(255, 214, 0);
+  ellipse(80, 80, 50);
+
+  // Ring 3: Black
+  stroke(0, 0, 0);
+  ellipse(110, 50, 50);
+
+  // Ring 4: Green
+  stroke(33, 176, 76);
+  ellipse(140, 80, 50);
+
+  // Ring 5:
+  stroke(234, 30, 35);
+  ellipse(170, 50, 50);
 }
